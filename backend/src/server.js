5979f4f50976500abc0f474bd647ab538e9e6f8c
daienelima <daiene.m17@gmail.com>
2019-10-02 21:36:17 -0300

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,8 +13,10 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3000);
